3537dcb164cba1096ca2eb343f9a97ce277e556b
LT <LT@qq.com>
2021-04-01 09:18:05 +0800

    hello第二次提交

---
+++ rep01/sb02/src/main/java/com/turing/sb02/entity/student.java
@@ -0,0 +1,44 @@
+package com.turing.sb02.entity;
+
+public class student {
+
+    private  String name;
+
+    private String school;
+
+    private String city;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
